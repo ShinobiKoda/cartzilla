cea339ea7bdabaeeeb7965b059991d2a2f0620cd
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-08-12 22:16:38 +0100

    feat: Add motion animations for enhanced visual effects on homepage

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { GoArrowUpRight } from "react-icons/go";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { zoomIn } from "./animations/motion";
 
 export function Homepage() {
   return (
     <div className="min-h-screen w-full bg-white dark:bg-[#181d25] px-4">
       <div className="w-full pt-6">
-        <div className="w-full bg-gradient-to-r from-gradient-start to-gradient-end dark:bg-gradient-to-r dark:from-dark-gradient-start dark:to-dark-gradient-end rounded-xl text-center">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={zoomIn}
+          className="w-full bg-gradient-to-r from-gradient-start to-gradient-end dark:bg-gradient-to-r dark:from-dark-gradient-start dark:to-dark-gradient-end rounded-xl text-center"
+        >
           <div className="flex flex-col gap-4 pt-12">
             <h3 className="text-gray-600 dark:text-gray-300 text-base font-normal">
               Feel the quality Sound
@@ -28,7 +35,7 @@ export function Homepage() {
             height={100}
             className="w-full"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
