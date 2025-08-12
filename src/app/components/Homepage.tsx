84265a951b8fe5a3553ae3ba97f3d2edb6e0bd63
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-08-12 21:57:55 +0100

    feat: Enhance homepage layout with improved button styling and add dark gradient colors

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -5,24 +5,30 @@ import Image from "next/image";
 export function Homepage() {
   return (
     <div className="min-h-screen w-full bg-white dark:bg-[#181d25] px-4">
-      <div className="w-full bg-gradient-to-r from-gradient-start to-gradient-end rounded-xl mt-6 text-center">
-        <div className="flex flex-col gap-4 pt-12">
-          <h3 className="text-gray-600 dark:text-gray-300 text-base font-normal">Feel the quality Sound</h3>
-          <p className="text-gray-900 dark:text-white font-semibold text-[34px]">
-            Headphone <br />
-            ProMax
-          </p>
-          <button>
-            Shop now <GoArrowUpRight />
-          </button>
+      <div className="w-full pt-6">
+        <div className="w-full bg-gradient-to-r from-gradient-start to-gradient-end dark:bg-gradient-to-r dark:from-dark-gradient-start dark:to-dark-gradient-end rounded-xl text-center">
+          <div className="flex flex-col gap-4 pt-12">
+            <h3 className="text-gray-600 dark:text-gray-300 text-base font-normal">
+              Feel the quality Sound
+            </h3>
+            <p className="text-gray-900 dark:text-white font-semibold text-[34px]">
+              Headphone <br />
+              ProMax
+            </p>
+            <div className="w-full max-w-[150px] mx-auto">
+              <button className="bg-button-primary text-white font-medium text-base rounded-lg flex items-center text-center py-4 w-full justify-center">
+                Shop now <GoArrowUpRight />
+              </button>
+            </div>
+          </div>
+          <Image
+            src="images/headphone.svg"
+            alt="Headphone"
+            width={100}
+            height={100}
+            className="w-full"
+          />
         </div>
-        <Image
-          src="images/headphone.svg"
-          alt="Headphone"
-          width={100}
-          height={100}
-          className="w-full"
-        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         'gradient-start': '#ACCBEE', // Corresponds to the 0% stop
         'gradient-end': '#E7F0FD',   // Corresponds to the 100% stop
         'button-primary': 'rgb(var(--button) / <alpha-value>)',
+        'dark-gradient-start': '#1B273A',
+        'dark-gradient-end': '#1F2632',
 
       }
     },
